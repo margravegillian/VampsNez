ed312f0edb5e2e0bc90107f39ca837a7f3750bd3
Dave Arneson <urvaius@gmail.com>
2016-04-27 20:57:02 -0700

    added nez

---
+++ VampsGame/Nez-PCL/Graphics/Renderers/ScreenSpaceRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Nez
+{
+	/// <summary>
+	/// Renderer that renders using its own Camera which doesnt move.
+	/// </summary>
+	public class ScreenSpaceRenderer : Renderer
+	{
+		public int[] renderLayers;
+
+
+		public ScreenSpaceRenderer( int renderOrder, params int[] renderLayers ) : base( renderOrder, null )
+		{
+			Array.Sort( renderLayers );
+			this.renderLayers = renderLayers;
+			camera = new ScreenSpaceCamera();
+			wantsToRenderAfterPostProcessors = true;
+		}
+
+
+		public override void render( Scene scene )
+		{
+			beginRender( camera );
+
+			for( var i = 0; i < renderLayers.Length; i++ )
+			{
+				var renderables = scene.renderableComponents.componentsWithRenderLayer( renderLayers[i] );
+				for( var j = 0; j < renderables.Count; j++ )
+				{
+					var renderable = renderables[j];
+					if( renderable.enabled )
+						renderAfterStateCheck( renderable, camera );
+				}
+			}
+
+			if( shouldDebugRender && Core.debugRenderEnabled )
+				debugRender( scene, camera );
+
+			endRender();
+		}
+
+	}
+}
+
